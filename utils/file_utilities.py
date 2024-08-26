cb827623b5ca113570cf3d79506cbac752271940
Christopher Carroll Smith <chriscarrollsmith@gmail.com>
2024-08-25 21:18:06 -0400

    docs: Add docstrings to a couple functions

--- utils/file_utilities.py
+++ utils/file_utilities.py
@@ -2,6 +2,13 @@ import hashlib
 
 
 def get_file_md5(filepath: str) -> str:
+    """Get the MD5 hash of a file
+
+    Args:
+        filepath: The path to the file
+
+    Returns:
+        The MD5 hash of the file"""
     md5_hash = hashlib.md5()
     with open(filepath, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
@@ -10,6 +17,14 @@ def get_file_md5(filepath: str) -> str:
 
 
 def format_file_size(size: int | float, suffix: str = "B") -> str:
+    """Convert file size to human readable format
+
+    Args:
+        size: The size of the file
+        suffix: The suffix to use for the file size
+
+    Returns:
+        The human readable file size"""
     for unit in ("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"):
         if abs(size) < 1024.0:
             return f"{size:3.1f}{unit}{suffix}"
